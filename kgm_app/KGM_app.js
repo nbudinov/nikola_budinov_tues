3aa508e0587d5d725789b6fcb06c98cc85af7414
M-Filipov <milko.filipov1@abv.bg>
2015-01-07 22:16:20 +0200

    add js

---
+++ kgm_app/KGM_app.js
@@ -0,0 +1,28 @@
+window.onload = function(){
+
+ };
+
+ // function drawImage(imageObj){
+ //      var canvas = document.getElementById("myCanvas");
+ //      var context = canvas.getContext("2d");
+ //      context.save();
+ //      context.translate(100,0);
+
+ //      var destX = 1;
+ //      var destY = 1;
+
+ //      context.drawImage(imageObj, destX, destY);
+
+ //      var imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+ //      var data = imageData.data;
+
+ //      for (var i = 0; i < data.length; i += 4) {
+ //          var red = data[i]; // red
+ //          var green = data[i + 1]; // green
+ //          var blue = data[i + 2]; // blue
+ //          // i+3 is alpha (the fourth element)
+ //      }
+
+ //      // overwrite original image
+ //      context.putImageData(imageData, 0, 0);
+ // }
